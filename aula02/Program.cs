ba2b5d50e8c289d70f61a750848ecfeeb6160746
eduardomatheus96 <eduardomatheusuesc@gmail.com>
2024-03-12 15:19:14 -0300

    Terminando Exercicio Lampada e Data

---
+++ aula02/Program.cs
@@ -0,0 +1,14 @@
+﻿using aula02;
+
+Lampada lamp = new Lampada(true);
+
+lamp.Imprimir();
+lamp.Desligar();
+lamp.Imprimir();
+
+
+Data data1 = new Data(12, 3, 2024);
+data1.Imprimir(Data.FORMATO_24H);
+
+Data data2 = new Data(12, 3, 2024, 15, 14, 7);
+data2.Imprimir(Data.FORMATO_12H);
