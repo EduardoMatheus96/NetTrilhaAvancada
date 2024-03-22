e219b8debded6fd4bb8011c63cf8d546ac768b0b
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-03-22 01:48:14 -0300

    Exercicio 02

---
+++ Atividade_03/Exercicio02/LinqConsultas.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercicio02;
+
+public class LinqConsultas
+{
+    public static List<ItemMercado> ItensHigieneOrdenadosPorPrecoDecrescente(List<ItemMercado> itens)
+    {
+        return itens
+            .Where(item => item.Tipo == Tipo.Higiene)
+            .OrderByDescending(item => item.Preco)
+            .ToList();
+    }
+
+    public static List<ItemMercado> ItensComPrecoMaiorQueCincoOrdenadosPorPreco(List<ItemMercado> itens)
+    {
+        return itens
+            .Where(item => item.Preco >= 5.00)
+            .OrderBy(item => item.Preco)
+            .ToList();
+    }
+
+    public static List<ItemMercado> ItensComidaOuBebidaOrdenadosPorNome(List<ItemMercado> itens)
+    {
+        return itens
+            .Where(item => item.Tipo == Tipo.Comida || item.Tipo == Tipo.Bebida)
+            .OrderBy(item => item.Nome)
+            .ToList();
+    }
+
+    public static List<TipoQuantidade> QuantidadeItensPorTipo(List<ItemMercado> itens)
+    {
+        return itens
+            .GroupBy(item => item.Tipo)
+            .Select(grupo => new TipoQuantidade
+            {
+                Tipo = grupo.Key,
+                Quantidade = grupo.Count()
+            })
+            .ToList();
+    }
+
+    public static List<TipoPreco> PrecosPorTipo(List<ItemMercado> itens)
+    {
+        return itens
+            .GroupBy(item => item.Tipo)
+            .Select(grupo => new TipoPreco
+            {
+                Tipo = grupo.Key,
+                PrecoMaximo = grupo.Max(item => item.Preco),
+                PrecoMinimo = grupo.Min(item => item.Preco),
+                MediaPreco = grupo.Average(item => item.Preco)
+            })
+            .ToList();
+    }
+}
---
+++ Atividade_03/Exercicio02/TipoPreco.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio02;
+
+public class TipoPreco
+{
+    public Tipo Tipo { get; set; }
+    public double PrecoMaximo { get; set; }
+    public double PrecoMinimo { get; set; }
+    public double MediaPreco { get; set; }
+}
